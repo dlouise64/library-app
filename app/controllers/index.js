577ad61c843df7518b626653e284d902c97e1c02
Dana Iti <dana@hashbang.co.nz>
2016-05-23 21:32:39 +1200

    Added email address observer

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  isDisabled: true
+  isDisabled: true,
+  
+  emailAddress: '',
+
+  actualEmailAddress: Ember.computed('emailAddress', function() { 
+    console.log('actualEmailAddress function is called: ', this.get('emailAddress'));
+  }),
+
+  emailAddressChanged: Ember.observer('emailAddress', function() { 
+    console.log('observer is called', this.get('emailAddress')); 
+  })
 });
